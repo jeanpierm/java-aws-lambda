a74caba88bf1e10f39d0f9f8df837f15e39d6366
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2022-11-19 15:19:23 -0500

    feat: add rest consume

--- src/main/java/org/example/Handler.java
+++ src/main/java/org/example/Handler.java
@@ -6,7 +6,15 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class Handler implements RequestHandler<Map<String, String>, String> {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -14,13 +22,26 @@ public class Handler implements RequestHandler<Map<String, String>, String> {
     @Override
     public String handleRequest(Map<String, String> event, Context context) {
         LambdaLogger logger = context.getLogger();
-        String response = "200 OK";
+        // Logger logger = Logger.getLogger(Handler.class.getName());
+        // String response = "200 OK";
         // log execution details
         logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
         logger.log("CONTEXT: " + gson.toJson(context));
         // process event
         logger.log("EVENT: " + gson.toJson(event));
         logger.log("EVENT TYPE: " + event.getClass());
-        return response;
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://jsonplaceholder.typicode.com/todos/1"))
+                    .GET()
+                    .build();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            logger.log("Respuesta obtenida: " + response.body());
+            return response.body();
+        } catch (URISyntaxException | IOException | InterruptedException exception) {
+            logger.log("Ocurrió un error: " + exception.getMessage());
+            return "ERROR: " + exception.getMessage();
+        }
     }
 }
---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		var handler = new Handler();
+		handler.handleRequest(null, null);
+	}
+}
